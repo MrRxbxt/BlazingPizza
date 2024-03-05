09afc331717e3c5b4168cc7d3ff57139ddd17fde
MrRxbxt <orville.online@gmail.com>
2024-03-06 07:36:32 +0800

    Upgrade from .net 7 to .net8; Use SQL Server database; Added dapper support;

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 using BlazingPizza.Data;
 using BlazingPizza.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpClient();
-builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+
+IConfiguration _configuration = builder.Configuration;
+var connectionString = _configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<PizzaStoreContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
